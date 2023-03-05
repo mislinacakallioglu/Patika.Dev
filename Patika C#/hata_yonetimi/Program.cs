fcd500fe6492fab2ec9b0272ec044a790b2ac5c4
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2023-03-05 17:09:54 +0300

    add

---
+++ Patika C#/hata_yonetimi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+ 
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //      Console.WriteLine("bir sayi giriniz: ");
+            // int sayi=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("girmis oldugunuz sayi:" + sayi);
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine("hata: " + ex.Message.ToString());
+                
+            // }
+            // // finally
+            // //{
+            // //     Console.Write("islem tamamlamdi. ");
+            // // }
+          
+          try
+          {
+            //int a = int.Parse(null);
+            //int a = int.Parse("test");
+            int a = int.Parse("12345678900");
+
+          }
+          catch(ArgumentNullException ex)
+          {
+                Console.WriteLine("bos deger giriniz: ");
+                Console.WriteLine(ex) ;
+          }
+          catch(FormatException ex){
+                 Console.WriteLine("veri tipi uygun degil ");
+                Console.WriteLine(ex) ;
+          }
+          catch(OverflowException ex){
+              Console.WriteLine("Cok kucuk ya da cok buyuk bir deger girdiniz. ");
+                Console.WriteLine(ex) ;
+          }
+          finally{
+            Console.WriteLine("islem basariyla tamamlandi.");
+          }
+        }
+    }
+}
---
+++ java101/src/Odev_FibonacciSerisi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Odev_FibonacciSerisi {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        int a,b,c;
+        System.out.print("Eleman sayisi girin: ");
+        a=inp.nextInt();
+
+        for (int i = 0; i <a ; i++)
+        {
+            a=0;
+            b=0;
+            c=a+b;
+        }
+
+    }
+}
