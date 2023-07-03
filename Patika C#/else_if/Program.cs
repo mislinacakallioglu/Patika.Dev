aa9a583395a2435bec961c9bc4e1f1e51b4bc500
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2023-07-03 14:21:13 +0300

    add

---
+++ Patika C#/else_if/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace else_if;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+            {
+                int time = DateTime.Now.Hour;
+                if(time>=6 && time<11)
+                {Console.WriteLine("gunaydin");}
+                else if(time<=18)
+                {Console.WriteLine("iyi günler");}
+                else
+                {Console.WriteLine("iyi geceler");}
+
+                string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+
+                sonuc= time>=6 && time<11  ?"gunyadin" : time<=18 ?" iyi gunler":"iyi geceler";
+                Console.WriteLine(sonuc);
+            }
+    }
+}
