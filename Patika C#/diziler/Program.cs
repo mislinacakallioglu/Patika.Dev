078c0d66cbe6d17ac6986ca881cf098d3701415b
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2023-09-14 16:29:41 +0300

    add

---
+++ Patika C#/diziler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace diziler{
+class Program
+{
+    static void Main (string[] args)
+        {
+            // dizi tanımlama
+            string [] renkler = new string[5];
+            string [] hayvanlar = {"kedi", "kopek", "kus","arı"};    
+
+            int [] dizi;
+            dizi= new int[5];
+            // dizilere değer atama ve erişim
+            renkler[0] = "Mavi" ;
+            dizi[3]=10;
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngülerle dizi kullanımı
+          // klavyeden girilen n sayının ortalamasını alma
+            Console.Write("Lutfen dizinin eleman sayisini giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lutfen {0}. sayiyi giriniz: " , i+1);
+                sayiDizisi[i]= int.Parse(Console.ReadLine());
+            }
+
+            int toplam=0;
+            foreach (var sayi in sayiDizisi)
+                toplam +=sayi;
+                Console.WriteLine("Ortalama: " + toplam/ diziUzunlugu);
+
+        }   
+    }
+}
