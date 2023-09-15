9ac0cc6e825c3c6d83edaf63e15ddd35f5b5a075
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2023-09-15 11:38:08 +0300

    add

---
+++ Patika C#/dizi_array_sinifi_metodlari/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace dizi_array_sınıfı_metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+
+            int [] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+            Console.WriteLine("- sirasiz liste - ");
+            
+            foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+            
+                Console.WriteLine("- sirali liste - ");
+                Array.Sort(sayiDizisi);
+                foreach (var sayi in sayiDizisi)
+                    Console.WriteLine(sayi);
+                // CLear 
+                // (sayi dizisi elemanlarını kullanarak ikinci indeksten itibaren iki tane elamanı sıfırlar)
+
+                Console.WriteLine("- array clear - ");
+                Array.Clear(sayiDizisi,2,2);
+
+                foreach (var sayi in sayiDizisi)
+                    Console.WriteLine(sayi);
+
+                // Reverse 
+                
+                Console.WriteLine("- Array Reverse - ");
+                Array.Reverse(sayiDizisi);
+
+                foreach (var sayi in sayiDizisi)
+                    Console.WriteLine(sayi);
+
+                //IndexOf
+                Console.WriteLine("- Array IndexOf - ");
+                Console.WriteLine( Array.IndexOf(sayiDizisi,23));
+            
+            // Resize
+            Console.WriteLine("- Resize - ");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=99;
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+        }
+    }
+}
