c21b6996b6b53347b10db2605af5c1901d4d60d5
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2024-05-17 12:29:15 +0300

    add

---
+++ java101/src/Bitirme_MayinTarlasi/MainFour.java
@@ -0,0 +1,11 @@
+package Bitirme_MayinTarlasi;
+import java.util.Scanner;
+import Bitirme_MayinTarlasi.MineSweeper;
+
+public class MainFour {
+    public static void main(String[] args) {
+        MineSweeper mine = new MineSweeper(5,5);
+        mine.run();
+
+    }
+}
